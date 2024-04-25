c7fcc14e8342a70c94e1e97cc9e86abfa953e98a
Leon <73612089+leonsaraqini@users.noreply.github.com>
2024-04-25 16:33:04 +0200

    Update Task_9.c

--- Structural Programming/KOL - 2/Task_9.c
+++ Structural Programming/KOL - 2/Task_9.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
 int Odd(int a[], int n){
-    if(n == 1){
-        if(a[n-1] % 2 == 0)
-            return 0;
-        else
-            return 1;
-        
+    if(n == 0){
+        return 0;
     }
-    
+
     if(a[n-1] % 2 == 0)
-        return Odd(a, n - 1) + 0;
+        return Odd(a, n - 1);
     else
         return Odd(a, n - 1) + 1;
 }
@@ -19,16 +15,16 @@ int main() {
     int n;
 
     scanf("%d", &n);
-    
+
     int a[n];
-    
+
     for(int i = 0; i < n; i++){
         scanf("%d", &a[i]);
     }
-    
+
     printf("%d", Odd(a,n));
-    
-    
+
+
 
     return 0;
 }
