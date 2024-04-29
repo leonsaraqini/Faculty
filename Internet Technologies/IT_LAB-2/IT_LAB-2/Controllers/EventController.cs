090747f695dc557ca84f08c143508594f397df00
Leon <73612089+leonsaraqini@users.noreply.github.com>
2024-04-29 16:27:35 +0200

    added Lab 2 IT exercise

---
+++ Internet Technologies/IT_LAB-2/IT_LAB-2/Controllers/EventController.cs
@@ -0,0 +1,83 @@
+﻿using IT_LAB_2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IT_LAB_2.Controllers
+{
+    public class EventController : Controller
+    {
+        private static List<Event> events = new List<Event>()
+        {
+            new Event() { Id = 1, Name = "School Reunion", Location = "Skopje"},
+            new Event() { Id = 2, Name = "Book Revision", Location = "Kumanovo"},
+            new Event() { Id = 3, Name = "DJ Party", Location = "Veles"}
+        };
+
+        // GET: Event
+        public ActionResult Index()
+        {
+            return View(events);
+        }
+
+        public ActionResult CreateNewEvent()
+        {
+            Event eventi = new Event();
+            return View(eventi);
+        }
+
+        [HttpPost]
+        public ActionResult CreateNewEvent(Event model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Id = events.Count() + 1;
+                events.Add(model);
+
+                return RedirectToAction("Index", "Event");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        public ActionResult EditEvent(int id)
+        {
+            Event eventi = events.Find(x => x.Id == id);
+
+            return View(eventi);
+        }
+
+        [HttpPost]
+        public ActionResult EditEvent(Event model)
+        {
+            if (ModelState.IsValid)
+            {
+                Event eventi = events.Find(x => x.Id == model.Id);
+                
+                eventi.Name = model.Name;
+                eventi.Location = model.Location;
+                
+                return RedirectToAction("Index", "Event");
+            }
+            else
+            {
+                return View(model);
+            }
+                   
+        }
+
+        public ActionResult DeleteEvent(int id)
+        {
+            Event eventi = events.Find(x => x.Id == id);
+
+            events.Remove(eventi);
+
+            return RedirectToAction("Index", "Event");
+        }
+    }
+}
---
+++ Internet Technologies/IT_LAB-2/IT_LAB-2/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IT_LAB_2.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [MinLength(5, ErrorMessage = "Smaller than 5!!!"), MaxLength(30, ErrorMessage = "Larger than 30!!!")]
+        public string Location { get; set; }
+    }
+}
